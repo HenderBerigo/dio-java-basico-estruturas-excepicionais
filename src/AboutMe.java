1987867ae0330693b9e04e8598defbbfcd195951
Hender Berigo <henderson.berigo@gmail.com>
2025-02-14 18:01:56 -0300

    commit inicial

---
+++ src/AboutMe.java
@@ -0,0 +1,11 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        
+    }
+}
